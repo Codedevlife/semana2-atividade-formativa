65c7a4b81d6d04c18c608f8d955afcb69f29ce75
Codedevlife <code.dev.life@gmail.com>
2025-03-06 16:10:16 -0300

    segundocommit
    Adicionado a função segundocommit

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# http://127.0.0.1:8000/segundocommit
+@app.get("/segundocommit")
+async def segundocommit():
+    return {"message": "segundocommit"}
+
+
 
